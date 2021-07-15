a4d42f7746f9cc61c57f7aff94089fd44b3ef514
ThreeArrowSucider <55866204+ThreeArrowSucider@users.noreply.github.com>
2021-07-15 23:23:40 +0900

    32bit対応

--- 1_Cppcli/source.cpp
+++ 1_Cppcli/source.cpp
@@ -1,49 +1,44 @@
 #include <windows.h>
-//#include <filesystem>
-//#include <string>
+#include <filesystem>
+#include <string>
 
 #define DllExport extern "C" __declspec(dllexport)
 
 // ä÷êîÉ|ÉCÉìÉ^å^
 typedef int(*FUNC1)(int num);
 
-//using wstr = std::wstring;
-//namespace fs = std::filesystem;
+using wstr = std::wstring;
+namespace fs = std::filesystem;
 
 
-//#ifdef _M_IX86 // 32bitîªíË
+#ifdef _M_IX86 // 32bitîªíË
+#endif
+DllExport int __stdcall wrap_32(int num)
+{
+    wstr dir = wstr(fs::current_path()) + L"\\0_CppProject32.dll";
 
-//DllExport void /*__stdcall*/ wrap_32()
-//{
-//    //wstr dir = wstr(fs::current_path()) + L"\\0_CppProject32.dll";
-//    wchar_t* dir = L"C:\\Users\\KOUDO\\source\\repos\\DLLSample\\2_CsMain\\bin\\Debug\\netcoreapp3.1\\0_CppProject32.dll";
-//    HMODULE hModule = LoadLibrary(dir/*.c_str()*/);
-//
-//    if (hModule == NULL)
-//        return /*-1*/;
-//
-//    FUNC1 func = (FUNC1)GetProcAddress(hModule, "Test");
-//
-//    //return func(num) + 32;
-//}
+    HMODULE hModule = LoadLibrary(dir.c_str());
 
-DllExport void __stdcall func() {
-    ;
+    if (hModule == NULL)
+        return -1;
+
+    FUNC1 func = (FUNC1)GetProcAddress(hModule, "Test");
+
+    return func(num) + 32;
 }
 
-////#else
-// 
-//DllExport int __stdcall wrap_64(int num) {
-//    wstr dir = wstr(fs::current_path()) + L"\\0_CppProject32.dll";
-//
-//    HMODULE hModule = LoadLibrary(dir.c_str());
-//
-//    if (hModule == NULL)
-//        return -1;
-//
-//    FUNC1 func = (FUNC1)GetProcAddress(hModule, "Test");
-//
-//    return func(num) + 64;
-//}
+ 
+DllExport int __stdcall wrap_64(int num) 
+{
+    wstr dir = wstr(fs::current_path()) + L"\\0_CppProject64.dll";
 
-//#endif
+    HMODULE hModule = LoadLibrary(dir.c_str());
+
+    if (hModule == NULL)
+        return -1;
+
+    FUNC1 func = (FUNC1)GetProcAddress(hModule, "Test");
+
+    return func(num) + 64;
+}
+
--- 2_CsMain/Program.cs
+++ 2_CsMain/Program.cs
@@ -6,21 +6,21 @@ namespace ConsoleApp1
 		delegate int Wrap( int num );
 		class Program
 		{
-				[DllImport( "1_Cppcli32.dll", EntryPoint = "func" )]
-				static extern void func( /*int num */);
+				[DllImport( @"1_Cppcli32.dll")]
+				static extern int wrap_32( int num );
 
-				//[DllImport( @"1_Cppcli64.dll" )]
-				//static extern int wrap_64( int num );
+				[DllImport( @"1_Cppcli64.dll" )]
+				static extern int wrap_64( int num );
 
 				static void Main( string[] args )
 				{
-						//Wrap wrap;
-						//if( Environment.Is64BitProcess )
-						//		wrap = wrap_64;
-						//else
-						//wrap = wrap_32;
-						func();
-						//Console.WriteLine( wrap_32( 100 ) );
+						Wrap wrap;
+						if( Environment.Is64BitProcess )
+								wrap = wrap_64;
+						else
+								wrap = wrap_32;
+						
+						Console.WriteLine( wrap_64( 100 ) );
 				}
 		}
 }
